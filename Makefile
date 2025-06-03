cb3b07cf780342ca5a869a8b95ccaeaf817ce93d
furrymonster <4urrym0nster@gmail.com>
2025-06-03 19:53:42 +0800

    line ?

--- Makefile
+++ Makefile
@@ -3,20 +3,39 @@ CPPFLAGS     =
 LDFLAGS      =
 LIBS         = -lm
 
+# 发布版本的编译选项
+RELEASE_FLAGS = -O3 -Wall
+# 调试版本的编译选项
+DEBUG_FLAGS   = -ggdb -g -pg -O0 -Wall -DDEBUG
+
 DESTDIR = ./
 TARGET  = main
+TARGET_DEBUG = $(TARGET)_debug
 
 OBJECTS := $(patsubst %.cpp,%.o,$(wildcard *.cpp))
 
-all: $(DESTDIR)$(TARGET)
+.PHONY: all clean debug release
+
+all: debug
+
+release: CPPFLAGS += $(RELEASE_FLAGS)
+release: $(DESTDIR)$(TARGET)
+
+debug: CPPFLAGS += $(DEBUG_FLAGS)
+debug: LDFLAGS += -pg 	# 使用gprof时，链接过程也需要-pg选项
+debug: $(DESTDIR)$(TARGET_DEBUG)
 
 $(DESTDIR)$(TARGET): $(OBJECTS)
-	$(SYSCONF_LINK) -Wall $(LDFLAGS) -o $(DESTDIR)$(TARGET) $(OBJECTS) $(LIBS)
+	$(SYSCONF_LINK) $(LDFLAGS) -o $(DESTDIR)$(TARGET) $(OBJECTS) $(LIBS)
+
+$(DESTDIR)$(TARGET_DEBUG): $(OBJECTS)
+	$(SYSCONF_LINK) $(LDFLAGS) -o $(DESTDIR)$(TARGET_DEBUG) $(OBJECTS) $(LIBS)
 
 $(OBJECTS): %.o: %.cpp
-	$(SYSCONF_LINK) -Wall $(CPPFLAGS) -c $(CFLAGS) $< -o $@
+	$(SYSCONF_LINK) $(CPPFLAGS) -c $(CFLAGS) $< -o $@
 
 clean:
 	-rm -f $(OBJECTS)
 	-rm -f $(TARGET)
+	-rm -f $(TARGET_DEBUG)
 	-rm -f *.tga
--- main.cpp
+++ main.cpp
@@ -1,22 +1,67 @@
 #include "tgaimage.h"
+#include <cstdlib>
 #include <iostream>
+#include <utility>
 
 void save_image(const TGAImage &image);
 
-void draw_line(int x0, int y0, int x1, int y1, TGAImage &image,
-               TGAColor color) {
-  for (float t = 0; t < 1; t += 0.1) {
+// too slow !
+void draw_line1(int x0, int y0, int x1, int y1, TGAImage &image,
+                TGAColor color) {
+  for (float t = 0.0f; t < 1.; t += 0.001f) {
     int x = x0 + (x1 - x0) * t;
     int y = y0 + (y1 - y0) * t;
     image.set(x, y, color);
   }
 }
 
+// only for not sleep and not inverse
+void draw_line2(int x0, int y0, int x1, int y1, TGAImage &image,
+                TGAColor color) {
+  for (int x = x0; x <= x1; x++) {
+    float t = (x - x0) / (float)(x1 - x0);
+    int y = y0 * (1. - t) + y1 * t;
+    image.set(x, y, color);
+  }
+}
+
+// best fit
+void draw_line3(int x0, int y0, int x1, int y1, TGAImage &image,
+                TGAColor color) {
+  bool steep = false;
+  if (std::abs(x0 - x1) <
+      std::abs(y0 - y1)) { // if the line is steep, we transpose the image
+    std::swap(x0, y0);
+    std::swap(x1, y1);
+    steep = true;
+  }
+  if (x0 > x1) { // make it left−to−right
+    std::swap(x0, x1);
+    std::swap(y0, y1);
+  }
+  for (int x = x0; x <= x1; x++) {
+    float t = (x - x0) / (float)(x1 - x0);
+    int y = y0 * (1. - t) + y1 * t;
+    if (steep) {
+      image.set(y, x, color); // if transposed, de−transpose
+    } else {
+      image.set(x, y, color);
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   TGAImage image(800, 800, TGAImage::RGB);
   TGAColor red(255, 0, 0, 255);
+  TGAColor green(0, 255, 0, 255);
+  TGAColor blue(0, 0, 255, 255);
 
-  draw_line(100, 100, 500, 700, image, red);
+  draw_line1(100, 100, 500, 700, image, red);
+  draw_line2(200, 200, 600, 300, image, blue);
+  draw_line3(0, 100, 100, 600, image, green);
+  draw_line3(500, 100, 100, 500, image, red);
+
+  image.flip_vertically();
 
   save_image(image);
   return 0;
